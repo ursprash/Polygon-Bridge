3b96f4f6ea99d33e5ce9cab5e2902b9e68d4f2bd
PRASHANT TIWARI <111697531+ursprash@users.noreply.github.com>
2023-07-27 12:54:15 +0530

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,14 +30,7 @@ const config: HardhatUserConfig = {
   },
   
 
-  circom: {
-    
-    inputBasePath: "./circuits",
-    // (required) The final ptau file, relative to inputBasePath, from a Phase 1 ceremony
-    ptau: "powersOfTau28_hez_final_12.ptau",
-    // (required) Each object in this array refers to a separate circuit
-    circuits: JSON.parse(JSON.stringify(circuits))
-  },
+  
 };
 
 export default config;
